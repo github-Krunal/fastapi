e6b91fb306de760d7d9b7346af838069ce520e27
githubkrunal <githubkrunal@gmail.com>
2025-09-21 19:50:05 +0530

    constant file created

---
+++ constant/api_constant.py
@@ -0,0 +1,6 @@
+# API related constants
+API_PREFIX = "/api"
+
+# Route url
+
+USER_REGISTRATION="/registration"
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 from model.registration import Registration
 from controller.auth_controller import register_user
-router = APIRouter(prefix="/api", tags=["Authentication"])
+from constant.api_constant import API_PREFIX,USER_REGISTRATION
+router = APIRouter(prefix=API_PREFIX, tags=["Authentication"])
 
-@router.post("/registration")
+@router.post(USER_REGISTRATION)
 async def register(registration: Registration):
     return register_user(registration)
