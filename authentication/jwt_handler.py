05d1ea8ee7c1d501e67db61a1de678476034e07f
githubkrunal <githubkrunal@gmail.com>
2025-12-02 09:04:17 +0530

    jwt token generation

---
+++ authentication/jwt_handler.py
@@ -0,0 +1,17 @@
+from datetime import timedelta, datetime
+from jose import jwt,JWTError
+from constant.jwt_constant import ALGORITHM, SECRET_KEY,ACCESS_TOKEN_EXPIRE_MINUTES
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
