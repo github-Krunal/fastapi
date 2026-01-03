f437fc34304855454d27fe3b6f377e623256da9d
githubkrunal <githubkrunal@gmail.com>
2026-01-03 19:38:56 +0530

    new changes

--- controller/auth_controller.py
+++ controller/auth_controller.py
@@ -1,17 +1,31 @@
+from fastapi import HTTPException
+from authentication.hashing import verify_password, hash_password
 from model.login import Login
 from model.registration  import Registration
 from database import userRegistrationCollection
+from  authentication.jwt_handler import create_access_token
+from  model.user import User
 from fastapi.responses import JSONResponse
 
 def register_user(registration: Registration):
-    result = userRegistrationCollection.insert_one(registration.dict())
-    return {"id": str(result.inserted_id), "message": "User registered"}
+    reg_dict = registration.model_dump()
+
+    # Hash the password
+    reg_dict["password"] = hash_password(reg_dict["password"])
+
+    # Save to DB
+    result = userRegistrationCollection.insert_one(reg_dict)
+
+    return {
+        "id": str(result.inserted_id),
+        "message": "User registered"
+    }
 
 def user_login(login: Login):
-    login_dict = login.dict()  # 👈 Convert to dict
+    login_dict = login.model_dump()  # 👈 Convert to dict
 
     # Find the user with matching email and password
-    user = userRegistrationCollection.find_one({
+    user:User = userRegistrationCollection.find_one({
         "$or": [
             {"email": login_dict["username"]},
             {"username": login_dict["username"]}
@@ -19,11 +33,19 @@ def user_login(login: Login):
         "password": login_dict["password"]
     })
 
-    if user:
-        user["_id"] = str(user["_id"])
-        return {"message": "Login successful", "user":user}
-    else:
-       return JSONResponse(
-        status_code=200,
-        content={"message": "Invalid email or password"}
-    )
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    if not verify_password(login_dict['password'], user['password']):
+        raise HTTPException(status_code=401, detail="Incorrect password")
+
+    token = create_access_token({"user_id": str(user['_id'])})
+
+    return {
+        "message": "Login successful",
+        "token": token,
+        "user": {
+            "_id": str(user['_id']),
+            "username": user.username
+        }
+    }
--- model/login.py
+++ model/login.py
@@ -6,3 +6,4 @@ from pydantic import BaseModel
 class Login(BaseModel):
     username: str
     password: str
+
